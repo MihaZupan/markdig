8041c243d87e8c58fa539735139c036171120ef9
Miha Zupan <mihazupan.zupan1@gmail.com>
2023-08-21 02:11:25 +0200

    Feed literal into IndexOfAny

--- src/Markdig/Polyfills/IndexOfHelpers.cs
+++ src/Markdig/Polyfills/IndexOfHelpers.cs
@@ -21,6 +21,21 @@ internal static class IndexOfHelpers
 
         return false;
     }
+
+#if !NETSTANDARD2_1_OR_GREATER
+    public static int IndexOfAny(this ReadOnlySpan<char> span, string values)
+    {
+        for (int i = 0; i < span.Length; i++)
+        {
+            if (values.Contains(span[i]))
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+#endif
 }
 
 #endif
